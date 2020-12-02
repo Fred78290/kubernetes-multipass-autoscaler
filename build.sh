e977cec289775b54a0e735b14771a7eef96c2b34
fboltz <frederic.boltz@gmail.com>
2020-12-02 13:28:57 +0100

    FEAT: Build kubernetes 1.20

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-make -e REGISTRY=fred78290 -e TAG=v1.19.0 container
+make -e REGISTRY=fred78290 -e TAG=v1.20.0-beta.2 container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=v1.20.0-beta.2 container -e GOARCH=arm64
