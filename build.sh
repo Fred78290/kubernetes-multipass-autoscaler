e6f9889ead846e27e60b58f318548e6d3f420d7f
fboltz <frederic.boltz@gmail.com>
2021-02-28 12:57:29 +0100

    Kubernetes v1.18.16

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make -e REGISTRY=fred78290 -e TAG=v1.18.12 container -e GOARCH=amd64
-make -e REGISTRY=fred78290 -e TAG=v1.18.12 container -e GOARCH=arm64
+make -e REGISTRY=fred78290 -e TAG=v1.18.16 container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=v1.18.16 container -e GOARCH=arm64
