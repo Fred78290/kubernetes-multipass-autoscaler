2bfea7a88f8e7441e7e9563aec460214e0b3eaa3
fboltz <frederic.boltz@gmail.com>
2021-02-28 12:24:50 +0100

    Update to kubernetes v1.20.4

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make -e REGISTRY=fred78290 -e TAG=v1.20.0-beta.2 container -e GOARCH=amd64
-make -e REGISTRY=fred78290 -e TAG=v1.20.0-beta.2 container -e GOARCH=arm64
+make -e REGISTRY=fred78290 -e TAG=v1.20.4 container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=v1.20.4 container -e GOARCH=arm64
