25d9910ec76181d83b1bc5ce31ad58cd77936ba9
fboltz <frederic.boltz@gmail.com>
2021-02-28 12:38:05 +0100

    Build arm64 arch

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make -e REGISTRY=fred78290 -e TAG=v1.19.4 container -e GOARCH=amd64
-make -e REGISTRY=fred78290 -e TAG=v1.19.4 container -e GOARCH=arm64
+make -e REGISTRY=fred78290 -e TAG=v1.19.7 container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=v1.19.7 container -e GOARCH=arm64
